b6c348a399594cfe6b3058e8ede317bcc0c783e7
sleepycat-web <amlansarmah26@gmail.com>
2026-06-10 23:09:43 +0530

    enable default dark mode, add spacing, style links, and clean up font sizes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${questrial.variable} h-full antialiased`}
+      className={`${questrial.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
